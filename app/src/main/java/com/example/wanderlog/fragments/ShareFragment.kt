4d3601fde7dc98bf6249147aeb744dd8511f60b0
SerbanTiurbe21 <serban.tiurbe@student.upt.ro>
2023-12-12 12:01:37 +0200

    created and populated share fragment

--- app/src/main/java/com/example/wanderlog/fragments/ShareFragment.kt
+++ app/src/main/java/com/example/wanderlog/fragments/ShareFragment.kt
@@ -1,26 +1,24 @@
 package com.example.wanderlog.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import com.example.wanderlog.R
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [ShareFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
+
 class ShareFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    private lateinit var shareButton: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,20 +32,13 @@ class ShareFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_share, container, false)
+        val view: View = inflater.inflate(R.layout.fragment_share, container, false)
+        initViews(view)
+        setShareButtonOnClickListener()
+        return view
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ShareFragment.
-         */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
             ShareFragment().apply {
@@ -56,5 +47,19 @@ class ShareFragment : Fragment() {
                     putString(ARG_PARAM2, param2)
                 }
             }
+    }
+
+    private fun initViews(view: View) {
+        shareButton = view.findViewById(R.id.iv_shareButton)
+    }
+
+    private fun setShareButtonOnClickListener() {
+        shareButton.setOnClickListener {
+            val intent = Intent(Intent.ACTION_SEND)
+            intent.type = "text/plain"
+            intent.putExtra(Intent.EXTRA_SUBJECT, "WanderLog")
+            intent.putExtra(Intent.EXTRA_TEXT, "WanderLog is a travel app that allows you to share your travel experiences with your friends and family. Download it now!")
+            startActivity(Intent.createChooser(intent, "Share using"))
+        }
     }
 }
