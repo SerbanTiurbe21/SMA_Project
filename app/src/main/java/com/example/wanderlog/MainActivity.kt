c32d2b32d381624be3a6e707233fe2fe95116ff8
SerbanTiurbe21 <serban.tiurbe@student.upt.ro>
2023-12-29 19:46:05 +0200

    - added rounded_background.xml for the level text - display the level based on the number of trips

--- app/src/main/java/com/example/wanderlog/MainActivity.kt
+++ app/src/main/java/com/example/wanderlog/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.navigation.ui.setupWithNavController
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
 import com.example.wanderlog.database.dto.UserDTO
+import com.example.wanderlog.database.models.Trip
 import com.example.wanderlog.databinding.ActivityMainBinding
 import com.google.gson.Gson
 
@@ -21,6 +22,8 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var userNameTextView: TextView
     private lateinit var userEmailTextView: TextView
+    private lateinit var ratingTextView: TextView
+    private lateinit var textViewLevel: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,10 +45,12 @@ class MainActivity : AppCompatActivity() {
         val headerView = navView.getHeaderView(0)
         userNameTextView = headerView.findViewById(R.id.textViewName)
         userEmailTextView = headerView.findViewById(R.id.textViewEmail)
+        ratingTextView = headerView.findViewById(R.id.ratingTextView)
+        textViewLevel = headerView.findViewById(R.id.textViewLevel)
 
         val currentUser: UserDTO? = retrieveCurrentUser()
         if (currentUser != null) {
-            updateUI(extractNameFromEmail(currentUser.email), currentUser.email)
+            updateUI(extractNameFromEmail(currentUser.email), currentUser.email, calculateTripsRating(currentUser.trips), getUserLevel(currentUser.trips.size))
         }
 
         setupActionBarWithNavController(navController, appBarConfiguration)
@@ -62,9 +67,11 @@ class MainActivity : AppCompatActivity() {
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 
-    private fun updateUI(name: String, email: String) {
+    private fun updateUI(name: String, email: String, rating: String, level: String) {
         userNameTextView.text = name
         userEmailTextView.text = email
+        ratingTextView.text = rating
+        textViewLevel.text = level
     }
 
     private fun extractNameFromEmail(email: String): String {
@@ -81,5 +88,23 @@ class MainActivity : AppCompatActivity() {
             null
         }
     }
+
+    private fun calculateTripsRating(trips: Set<Trip>): String {
+        var rating = 0.0
+        for (trip in trips) {
+            rating += trip.rating
+        }
+        return (rating / trips.size).toString()
+    }
+
+    private fun getUserLevel(tripCount: Int): String {
+        return when {
+            tripCount >= 15 -> "Platinum Level"
+            tripCount >= 10 -> "Gold Level"
+            tripCount >= 5-> "Silver Level"
+            else -> "Bronze Level"
+        }
+    }
+
 
 }
